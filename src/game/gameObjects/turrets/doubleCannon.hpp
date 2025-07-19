d71a44550cbe84425adf3d2009d077a8844b1fc3
Pirko231 <szymek.pirowski@gmail.com>
2025-07-19 20:15:11 +0200

    Dodanie double cannon (implementacja)

--- src/game/gameObjects/turrets/doubleCannon.hpp
+++ src/game/gameObjects/turrets/doubleCannon.hpp
@@ -3,7 +3,15 @@
 
 class DoubleCannon : public Turret
 {
+    int maxCooldown2{80};
+    int cooldown2{maxCooldown - 20}; // cooldown2 odpowiada za drugi strzal
 public:
     DoubleCannon();
     void shoot(sf::Vector2f, Enemy*) override;
+private:
+    /// @brief pierwszy strzal
+    void barrelOneShoot(sf::Vector2f from, Enemy * target);
+
+    /// @brief drugi strzal
+    void barrelSecondShoot(sf::Vector2f from, Enemy * target);
 };
