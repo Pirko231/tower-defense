e79270035697d759d3f1c9fb30200c225ce3ca5e
Pirko231 <szymek.pirowski@gmail.com>
2025-07-26 12:48:42 +0200

    struktura Turret::Stats

--- src/game/gameObjects/turrets/turret.cpp
+++ src/game/gameObjects/turrets/turret.cpp
@@ -2,11 +2,5 @@
 
 void Turret::update()
 {
-    /*std::erase_if(bullets, [](const BasicBullet& b)->bool{return b.shouldDelete();});
-    for(auto& bullet : bullets)
-    {
-        bullet.update();
-        bullet.hitTarget();
-    }*/
 }
 
--- src/game/gameObjects/turrets/turret.hpp
+++ src/game/gameObjects/turrets/turret.hpp
@@ -6,31 +6,49 @@
 
 class Turret : public sf::Drawable, public sf::Transformable
 {
+public:
+    struct Stats
+    {
+        float range{};
+        int damage{};
+    };
+private:
     sf::Sprite sprite;
-    float range{};
+    int upgradePrice{};
 protected:
     IBulletManager* bulletManager{};
 
+    
     int maxCooldown{100};
     int cooldown{};
-    int damage{};
+    Stats stats;
 public:
     virtual void update();
 
     /// @brief oblicza i zwraca obrazenia na klatke
-    virtual float getDPS() const {return (float)damage / (float)maxCooldown;}
+    virtual float getDPS() const {return (float)stats.damage / (float)maxCooldown;}
 
-    float getRange() const {return range;}
+    float getRange() const {return stats.range;}
 
     sf::FloatRect getGlobalBounds() const {return sf::FloatRect{getPosition(), sprite.getGlobalBounds().size};}
 
     virtual void shoot(sf::Vector2f from, Enemy* target) = 0;
+
+    int getUpgradePrice() const {return upgradePrice;}
+
+    /// @brief mnozy statyskyki o doubler
+    void upgrade(Stats statsDoubler, float priceDoubler)
+    {
+        stats.range *= statsDoubler.range;
+        stats.damage *= statsDoubler.damage;
+        upgradePrice = (float)upgradePrice * priceDoubler;
+    }
 protected:
     explicit Turret(const sf::Texture& _texture, IBulletManager* _bulletManager, float _range, int _damage)
-        : sprite(_texture), range{_range}, bulletManager(_bulletManager),  cooldown{maxCooldown}, damage{_damage}
+        : sprite(_texture), bulletManager(_bulletManager),  cooldown{maxCooldown}, stats(_range, _damage)
     {}
 
-    void setRange(float _range) {range = _range;}
+    void setRange(float _range) {stats.range = _range;}
 
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const override
     {
